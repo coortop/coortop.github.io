07b58528ea9fc03fbfce7a094ce7d82f1ce4e985
coortop <coor@coor.top>
2020-02-18 11:53:56 +0800

    Site updated: 2020-02-18 11:53:56

--- custom/js/aplayer/allPage-fixed.js
+++ custom/js/aplayer/allPage-fixed.js
@@ -1,1 +1,1 @@
-$(function(){var e=new APlayer({container:document.getElementById("aplayer-allPage-fixed"),lrcType:3,order:"list",fixed:!0});$.getJSON("https://api.i-meto.com/meting/api?type=playlist&id=3034682013",function(t){e.list.add(t)})});
+$(function(){var e=new APlayer({container:document.getElementById("aplayer-allPage-fixed"),lrcType:3,order:"list",fixed:!0});$.getJSON("https://api.fczbl.vip/163/?type=playlist&id=3034682013",function(t){e.list.add(t)})});
